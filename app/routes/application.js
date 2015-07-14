96834b1b951610418c489fd3186f9f6dcb50fdf2
santuan <Mbiondo>
2015-07-14 20:57:53 -0300

    Se maqueto la app

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,5 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 
+	actions: {
+		close: function () {
+			var gui = require('nw.gui');
+			gui.Window.get().leaveKioskMode();
+			gui.App.quit();
+		}		
+	}
 
 });
